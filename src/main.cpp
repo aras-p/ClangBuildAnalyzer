aras-p/ClangBuildAnalyzer

commit d1adb6a861b55feefe4c0f8aac88c3b058d33f9e

--stop: Handle fwrite() failure
This handles the case where the file could be opened, but not completely
written to, e.g. if there is not enough space on the device.

Closes #2.

src/main.cpp
@@ -228,7 +228,14 @@ static int RunStop(int argc, const char* argv[])
         printf("%sERROR: failed to write result file '%s'.%s\n", col::kRed, outFile.c_str(), col::kReset);
         return 1;
     }
-    fwrite(bigJson.data(), 1, bigJson.size(), fout);
+    const size_t numBytesWritten = fwrite(bigJson.data(), 1, bigJson.size(), fout);
+    if (numBytesWritten != bigJson.size())
+    {
+        printf("%sERROR: failed to write result file '%s', %llu of %llu bytes written.%s\n",
+            col::kRed, outFile.c_str(), static_cast<unsigned long long>(numBytesWritten), static_cast<unsigned long long>(bigJson.size()), col::kReset);
+        fclose(fout);
+        return 1;
+    }
     fclose(fout);
 
     double tDuration = stm_sec(stm_since(tStart));

commit bd65065481b1aff6ea5d5fb3e0305c92d7265085

Use %zu printf format specifier for size_t
As noted by @aras-p in #3

Fixes: d1adb6a861b55feefe4c0f8aac88c3b058d33f9e

src/main.cpp
@@ -231,8 +231,8 @@ static int RunStop(int argc, const char* argv[])
     const size_t numBytesWritten = fwrite(bigJson.data(), 1, bigJson.size(), fout);
     if (numBytesWritten != bigJson.size())
     {
-        printf("%sERROR: failed to write result file '%s', %llu of %llu bytes written.%s\n",
-            col::kRed, outFile.c_str(), static_cast<unsigned long long>(numBytesWritten), static_cast<unsigned long long>(bigJson.size()), col::kReset);
+        printf("%sERROR: failed to write result file '%s', %zu of %zu bytes written.%s\n",
+            col::kRed, outFile.c_str(), numBytesWritten, bigJson.size(), col::kReset);
         fclose(fout);
         return 1;
     }