1eb6ba944374256a17374b2d75a1f53246848173
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-04-21 03:35:46 +0100

    Fix build on MinGW (MSYS2)

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ static int RunStart(int argc, const char* argv[])
     return 0;
 }
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__MINGW32__)
 static time_t FiletimeToTime(const FILETIME& ft)
 {
     ULARGE_INTEGER ull;
@@ -129,7 +129,7 @@ struct JsonFileFinder
         if (!cf_get_file_time(f->path, &mtime))
             return;
         time_t fileModTime;
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__MINGW32__)
         fileModTime = FiletimeToTime(mtime.time);
 #else
         fileModTime = mtime.time;
