d3f3a4b23a035ce12a1585a7dc8d1f60f6aeaf55
Aras Pranckevicius <aras@nesnausk.org>
2024-02-21 10:36:57 +0200

    Tests: add test for clang 16

---
+++ tests/self-win-clang-16.0/produceJsonFiles.cmd
@@ -0,0 +1,8 @@
+del *.json
+pushd ..\..
+mkdir build\clang
+"%PROGRAMFILES%\LLVM\bin\clang" -std=c++17 -O2 -ftime-trace -c src/BuildEvents.cpp -o build/clang/BuildEvents.o
+"%PROGRAMFILES%\LLVM\bin\clang" -std=c++17 -O2 -ftime-trace -c src/main.cpp -o build/clang/main.o
+popd
+copy /Y ..\..\build\clang\*.json .
+pause
